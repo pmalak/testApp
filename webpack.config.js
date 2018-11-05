a4a7d4970ad1cf909883166efa6a7acda0f1d067
Petr Malak <petrmalak88@gmail.com>
2018-11-05 12:09:10 +0100

    initial and fixing babel

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
